6600d0e747ac847d6956b0eb68f1e8bb04964e88
5jisoo <56earls@gmail.com>
2024-02-05 17:45:32 +0900

    initial project setting

---
+++ src/main/java/com/gdsc/remine/RemineApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.remine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdsc/remine/RemineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.remine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
