c2bc9820f75339dc4856fc569cb4eaa683adfe58
5jisoo <56earls@gmail.com>
2024-02-11 01:09:29 +0900

    #3 Feat: get my-page

--- src/main/java/com/gdsc/remine/member/controller/MemberController.java
+++ src/main/java/com/gdsc/remine/member/controller/MemberController.java
@@ -1,11 +1,21 @@
 package com.gdsc.remine.member.controller;
 
+import com.gdsc.remine.global.api_payload.ApiResponse;
+import com.gdsc.remine.member.dto.response.MyPageResponse;
 import com.gdsc.remine.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
 public class MemberController {
     private final MemberService memberService;
+
+    @GetMapping("")
+    public ApiResponse<MyPageResponse> getMyPage() {
+        return ApiResponse.onSuccess(memberService.getMyPage());
+    }
 }
